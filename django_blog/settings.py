3c1d118514ab4ee1cbb029fdf164b98591cb340a
Leigh Forrest <leighmforrest@gmail.com>
2019-01-06 18:50:10 -0500

    Up to and including video 8

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+# MEDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
